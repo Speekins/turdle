85b957e4fd366eb8c62a12eeb3cced95c1eec57a
Spencer Haka <spencer.haka@gmail.com>
2022-10-27 13:49:03 -0600

    Refactor end of game scenario to display message in loss scenario

--- index.js
+++ index.js
@@ -1,256 +1,265 @@
 // Global Variables
-var winningWord = '';
-var currentRow = 1;
-var guess = '';
-var gamesPlayed = [];
-let words;
+var winningWord = ''
+var currentRow = 1
+var guess = ''
+var gamesPlayed = []
+let words
 
 // Query Selectors
-var inputs = document.querySelectorAll('input');
-var guessButton = document.querySelector('#guess-button');
-var keyLetters = document.querySelectorAll('span');
-var errorMessage = document.querySelector('#error-message');
-var viewRulesButton = document.querySelector('#rules-button');
-var viewGameButton = document.querySelector('#play-button');
-var viewStatsButton = document.querySelector('#stats-button');
-var gameBoard = document.querySelector('#game-section');
-var letterKey = document.querySelector('#key-section');
-var rules = document.querySelector('#rules-section');
-var stats = document.querySelector('#stats-section');
-var gameOverBox = document.querySelector('#game-over-section');
-var gameOverGuessCount = document.querySelector('#game-over-guesses-count');
-var gameOverGuessGrammar = document.querySelector('#game-over-guesses-plural');
+var inputs = document.querySelectorAll('input')
+var guessButton = document.querySelector('#guess-button')
+var keyLetters = document.querySelectorAll('span')
+var errorMessage = document.querySelector('#error-message')
+var viewRulesButton = document.querySelector('#rules-button')
+var viewGameButton = document.querySelector('#play-button')
+var viewStatsButton = document.querySelector('#stats-button')
+var gameBoard = document.querySelector('#game-section')
+var letterKey = document.querySelector('#key-section')
+var rules = document.querySelector('#rules-section')
+var stats = document.querySelector('#stats-section')
+var gameOverBoxWin = document.querySelector('#game-over-section-win')
+var gameOverBoxLoss = document.querySelector('#game-over-section-loss')
+var gameOverGuessCount = document.querySelector('#game-over-guesses-count')
+var gameOverGuessGrammar = document.querySelector('#game-over-guesses-plural')
 
 // Event Listeners
 window.addEventListener('load', () => {
   fetch('http://localhost:3001/api/v1/words')
-  .then(response => response.json())
-  .then(data => {
-    words = data;
-    setGame();
-  })
-});
+    .then(response => response.json())
+    .then(data => {
+      words = data
+      setGame()
+    })
+})
 
 for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('keyup', function() { moveToNextInput(event) });
+  inputs[i].addEventListener('keyup', function () { moveToNextInput(event) })
 }
 
 for (var i = 0; i < keyLetters.length; i++) {
-  keyLetters[i].addEventListener('click', function() { clickLetter(event) });
+  keyLetters[i].addEventListener('click', function () { clickLetter(event) })
 }
 
-guessButton.addEventListener('click', submitGuess);
+guessButton.addEventListener('click', submitGuess)
 
-viewRulesButton.addEventListener('click', viewRules);
+viewRulesButton.addEventListener('click', viewRules)
 
-viewGameButton.addEventListener('click', viewGame);
+viewGameButton.addEventListener('click', viewGame)
 
-viewStatsButton.addEventListener('click', viewStats);
+viewStatsButton.addEventListener('click', viewStats)
 
 // Functions
 function setGame() {
-  currentRow = 1;
-  winningWord = getRandomWord();
-  console.log(winningWord);
-  updateInputPermissions();
+  currentRow = 1
+  winningWord = getRandomWord()
+  console.log(winningWord)
+  updateInputPermissions()
 }
 
 function getRandomWord() {
-  let wordsTotal = words.length;
-  var randomIndex = Math.floor(Math.random() * wordsTotal);
-  return words[randomIndex];
+  let wordsTotal = words.length
+  var randomIndex = Math.floor(Math.random() * wordsTotal)
+  return words[randomIndex]
 }
 
 function updateInputPermissions() {
-  for(var i = 0; i < inputs.length; i++) {
-    if(!inputs[i].id.includes(`-${currentRow}-`)) {
-      inputs[i].disabled = true;
+  for (var i = 0; i < inputs.length; i++) {
+    if (!inputs[i].id.includes(`-${currentRow}-`)) {
+      inputs[i].disabled = true
     } else {
-      inputs[i].disabled = false;
+      inputs[i].disabled = false
     }
   }
 
-  inputs[0].focus();
+  inputs[0].focus()
 }
 
 function moveToNextInput(e) {
-  var key = e.keyCode || e.charCode;
+  var key = e.keyCode || e.charCode
 
-  if( key !== 8 && key !== 46 ) {
-    var indexOfNext = parseInt(e.target.id.split('-')[2]) + 1;
-    inputs[indexOfNext].focus();
+  if (key !== 8 && key !== 46) {
+    var indexOfNext = parseInt(e.target.id.split('-')[2]) + 1
+    if (!inputs[indexOfNext]) {return}
+    inputs[indexOfNext].focus()
   }
 }
 
 function clickLetter(e) {
-  var activeInput = null;
-  var activeIndex = null;
+  var activeInput = null
+  var activeIndex = null
 
   for (var i = 0; i < inputs.length; i++) {
-    if(inputs[i].id.includes(`-${currentRow}-`) && !inputs[i].value && !activeInput) {
-      activeInput = inputs[i];
-      activeIndex = i;
+    if (inputs[i].id.includes(`-${currentRow}-`) && !inputs[i].value && !activeInput) {
+      activeInput = inputs[i]
+      activeIndex = i
     }
   }
 
-  activeInput.value = e.target.innerText;
-  inputs[activeIndex + 1].focus();
+  activeInput.value = e.target.innerText
+  inputs[activeIndex + 1].focus()
 }
 
 function submitGuess() {
   if (checkIsWord()) {
-    errorMessage.innerText = '';
-    compareGuess();
-    if (checkForWin()) {
-      setTimeout(declareWinner, 1000);
+    errorMessage.innerText = ''
+    compareGuess()
+    if (checkForWin() || currentRow === 6) {
+      setTimeout(declareWinner, 1000)
     } else {
-      changeRow();
+      changeRow()
     }
   } else {
-    errorMessage.innerText = 'Not a valid word. Try again!';
+    errorMessage.innerText = 'Not a valid word. Try again!'
   }
 }
 
 function checkIsWord() {
-  guess = '';
+  guess = ''
 
-  for(var i = 0; i < inputs.length; i++) {
-    if(inputs[i].id.includes(`-${currentRow}-`)) {
-      guess += inputs[i].value;
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].id.includes(`-${currentRow}-`)) {
+      guess += inputs[i].value
     }
   }
 
-  return words.includes(guess);
+  return words.includes(guess)
 }
 
 function compareGuess() {
-  var guessLetters = guess.split('');
+  var guessLetters = guess.split('')
 
   for (var i = 0; i < guessLetters.length; i++) {
 
     if (winningWord.includes(guessLetters[i]) && winningWord.split('')[i] !== guessLetters[i]) {
-      updateBoxColor(i, 'wrong-location');
-      updateKeyColor(guessLetters[i], 'wrong-location-key');
+      updateBoxColor(i, 'wrong-location')
+      updateKeyColor(guessLetters[i], 'wrong-location-key')
     } else if (winningWord.split('')[i] === guessLetters[i]) {
-      updateBoxColor(i, 'correct-location');
-      updateKeyColor(guessLetters[i], 'correct-location-key');
+      updateBoxColor(i, 'correct-location')
+      updateKeyColor(guessLetters[i], 'correct-location-key')
     } else {
-      updateBoxColor(i, 'wrong');
-      updateKeyColor(guessLetters[i], 'wrong-key');
+      updateBoxColor(i, 'wrong')
+      updateKeyColor(guessLetters[i], 'wrong-key')
     }
   }
 
 }
 
 function updateBoxColor(letterLocation, className) {
-  var row = [];
+  var row = []
 
   for (var i = 0; i < inputs.length; i++) {
-    if(inputs[i].id.includes(`-${currentRow}-`)) {
-      row.push(inputs[i]);
+    if (inputs[i].id.includes(`-${currentRow}-`)) {
+      row.push(inputs[i])
     }
   }
 
-  row[letterLocation].classList.add(className);
+  row[letterLocation].classList.add(className)
 }
 
 function updateKeyColor(letter, className) {
-  var keyLetter = null;
+  var keyLetter = null
 
   for (var i = 0; i < keyLetters.length; i++) {
     if (keyLetters[i].innerText === letter) {
-      keyLetter = keyLetters[i];
+      keyLetter = keyLetters[i]
     }
   }
 
-  keyLetter.classList.add(className);
+  keyLetter.classList.add(className)
 }
 
 function checkForWin() {
-  return guess === winningWord;
+  return guess === winningWord
 }
 
 function changeRow() {
-  currentRow++;
-  updateInputPermissions();
+  currentRow++
+  updateInputPermissions()
 }
 
 function declareWinner() {
-  recordGameStats();
-  changeGameOverText();
-  viewGameOverMessage();
-  setTimeout(startNewGame, 4000);
+  recordGameStats()
+  changeGameOverText()
+  viewGameOverMessage()
+  setTimeout(startNewGame, 4000)
 }
 
 function recordGameStats() {
-  gamesPlayed.push({ solved: true, guesses: currentRow });
+  gamesPlayed.push({ solved: true, guesses: currentRow })
+  console.log(gamesPlayed)
 }
 
 function changeGameOverText() {
-  gameOverGuessCount.innerText = currentRow;
+  gameOverGuessCount.innerText = currentRow
   if (currentRow < 2) {
-    gameOverGuessGrammar.classList.add('collapsed');
+    gameOverGuessGrammar.classList.add('collapsed')
   } else {
-    gameOverGuessGrammar.classList.remove('collapsed');
+    gameOverGuessGrammar.classList.remove('collapsed')
   }
 }
 
 function startNewGame() {
-  clearGameBoard();
-  clearKey();
-  setGame();
-  viewGame();
-  inputs[0].focus();
+  clearGameBoard()
+  clearKey()
+  setGame()
+  viewGame()
+  inputs[0].focus()
 }
 
 function clearGameBoard() {
   for (var i = 0; i < inputs.length; i++) {
-    inputs[i].value = '';
-    inputs[i].classList.remove('correct-location', 'wrong-location', 'wrong');
+    inputs[i].value = ''
+    inputs[i].classList.remove('correct-location', 'wrong-location', 'wrong')
   }
 }
 
 function clearKey() {
   for (var i = 0; i < keyLetters.length; i++) {
-    keyLetters[i].classList.remove('correct-location-key', 'wrong-location-key', 'wrong-key');
+    keyLetters[i].classList.remove('correct-location-key', 'wrong-location-key', 'wrong-key')
   }
 }
 
 // Change Page View Functions
 
 function viewRules() {
-  letterKey.classList.add('hidden');
-  gameBoard.classList.add('collapsed');
-  rules.classList.remove('collapsed');
-  stats.classList.add('collapsed');
-  viewGameButton.classList.remove('active');
-  viewRulesButton.classList.add('active');
-  viewStatsButton.classList.remove('active');
+  letterKey.classList.add('hidden')
+  gameBoard.classList.add('collapsed')
+  rules.classList.remove('collapsed')
+  stats.classList.add('collapsed')
+  viewGameButton.classList.remove('active')
+  viewRulesButton.classList.add('active')
+  viewStatsButton.classList.remove('active')
 }
 
 function viewGame() {
-  letterKey.classList.remove('hidden');
-  gameBoard.classList.remove('collapsed');
-  rules.classList.add('collapsed');
-  stats.classList.add('collapsed');
-  gameOverBox.classList.add('collapsed')
-  viewGameButton.classList.add('active');
-  viewRulesButton.classList.remove('active');
-  viewStatsButton.classList.remove('active');
+  letterKey.classList.remove('hidden')
+  gameBoard.classList.remove('collapsed')
+  rules.classList.add('collapsed')
+  stats.classList.add('collapsed')
+  viewGameButton.classList.add('active')
+  viewRulesButton.classList.remove('active')
+  viewStatsButton.classList.remove('active')
+  gameOverBoxWin.classList.add('collapsed')
+  gameOverBoxLoss.classList.add('collapsed')
 }
 
 function viewStats() {
-  letterKey.classList.add('hidden');
-  gameBoard.classList.add('collapsed');
-  rules.classList.add('collapsed');
-  stats.classList.remove('collapsed');
-  viewGameButton.classList.remove('active');
-  viewRulesButton.classList.remove('active');
-  viewStatsButton.classList.add('active');
+  letterKey.classList.add('hidden')
+  gameBoard.classList.add('collapsed')
+  rules.classList.add('collapsed')
+  stats.classList.remove('collapsed')
+  viewGameButton.classList.remove('active')
+  viewRulesButton.classList.remove('active')
+  viewStatsButton.classList.add('active')
 }
 
 function viewGameOverMessage() {
-  gameOverBox.classList.remove('collapsed')
-  letterKey.classList.add('hidden');
-  gameBoard.classList.add('collapsed');
+  if (currentRow === 6) {
+    console.log(currentRow);
+    gameOverBoxLoss.classList.remove('collapsed')
+  } else {
+    gameOverBoxWin.classList.remove('collapsed')
+  }
+  letterKey.classList.add('hidden')
+  gameBoard.classList.add('collapsed')
 }
